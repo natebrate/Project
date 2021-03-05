05f87c32681d70cf8c4d5cbb4e47b1dccef2f1c3
Nate Brate <62619983+natebrate@users.noreply.github.com>
2021-03-05 11:16:27 -0400

    images

--- store/models.py
+++ store/models.py
@@ -3,7 +3,20 @@ from django.db import models
 # Create your models here.
 
 from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.models import Image, AbstractImage, AbstractRendition
+
+
+class CustomImage(AbstractImage):
+    # Add any extra fields to image here
+
+    # eg. To add a caption field:
+    # caption = models.CharField(max_length=255, blank=True)
+
+    admin_form_fields = Image.admin_form_fields + (
+        'banner_image',
+    )
 
 
 class StorePage(Page):
@@ -11,8 +24,16 @@ class StorePage(Page):
         max_length=100, default="Store Front!"
     )
     introduction = models.TextField(blank=True)
+    banner_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel("banner_title"),
-        FieldPanel("introduction")
+        FieldPanel("introduction"),
+        ImageChooserPanel("banner_image"),
     ]
